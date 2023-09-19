81e42b1d084e75008138a2ef55600c1639ace734
Mohammad Jabir <mohammadj94@gmail.com>
2023-09-19 09:43:31 -0600

    demo complete.

--- WrapUpDemo/Program.cs
+++ WrapUpDemo/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         List<PersonModel> people = new List<PersonModel>
         {
-            new PersonModel {FirstName = "Tim", LastName = "Corey", Email = "timc@gmail.com"},
+            new PersonModel {FirstName = "Tim", LastName = "Coreydarnit", Email = "timc@gmail.com"},
             new PersonModel {FirstName = "Bob", LastName = "Jackson", Email = "bobj@gmail.com"},
             new PersonModel {FirstName = "Quack", LastName = "Malone", Email = "quackm@gmail.com"}
         };
@@ -14,17 +14,39 @@ class Program
         List<CarModel> cars = new List<CarModel>
         {
             new CarModel {Manufacturer = "Toyota", Model = "Corolla"},
-            new CarModel {Manufacturer = "Toyota", Model = "Highlandeer"},
+            new CarModel {Manufacturer = "Toyotaheck", Model = "Highlander"},
             new CarModel {Manufacturer = "Ford", Model = "Mustang"}
         };
 
+
+        DataAccess<PersonModel> peopleData = new DataAccess<PersonModel>();
+        peopleData.BadEntryFound += PeopleData_BadEntryFound;
+        peopleData.SaveToCSV(people, @"C:\Users\moham\OneDrive\Documents\a_Programming\IAMTIMCOREY\C#_MASTER_COURSE\6.8_MiniProject_Generics_Events\WrapUpDemoApp\people.csv");
+        
+        DataAccess<CarModel> carData = new DataAccess<CarModel>();
+        carData.BadEntryFound += CarData_BadEntryFound;
+        carData.SaveToCSV(cars, @"C:\Users\moham\OneDrive\Documents\a_Programming\IAMTIMCOREY\C#_MASTER_COURSE\6.8_MiniProject_Generics_Events\WrapUpDemoApp\cars.csv");
+
         Console.ReadLine();
+    }
+
+    private static void CarData_BadEntryFound(object? sender, CarModel e)
+    {
+        Console.WriteLine($"Bad entry found for {e.Manufacturer} {e.Model}");
+    }
+
+    private static void PeopleData_BadEntryFound(object? sender, PersonModel e)
+    {
+        Console.WriteLine($"Bad entry found for {e.FirstName} {e.LastName}");
     }
 }
 
-public static class DataAccess
+public class DataAccess<T> where T : new()
 {
-    public static void SaveToCSV<T> (this List<T> items) where T : new()
+    public event EventHandler<T> BadEntryFound;
+
+    // using reflection
+    public void SaveToCSV(List<T> items, string filePath)
     {
         List<string> rows = new List<string>();
         T entry = new T();
@@ -33,9 +55,49 @@ public static class DataAccess
         string row = "";
         foreach (var col in cols)
         {
-            row += $",{col.Name}"; 
+            row += $",{col.Name}";
         }
         row = row.Substring(1);
         rows.Add(row);
+
+        foreach (var item in items)
+        {
+            row = "";
+            bool badWordDetected = false;
+            foreach (var col in cols)
+            {
+                string val = col.GetValue(item, null).ToString();
+
+                badWordDetected = BadWordDetector(val);
+                if (badWordDetected)
+                {
+                    BadEntryFound?.Invoke(this, item);
+                    break;
+                }
+
+                row += $",{val}";
+            }
+
+            if (!badWordDetected)
+            {
+                row = row.Substring(1);
+                rows.Add(row);
+            }
+        }
+
+        File.WriteAllLines(filePath, rows);
+    }
+
+    private bool BadWordDetector(string stringToTest)
+    {
+        bool output = false;
+        string lowerCaseTest = stringToTest.ToLower();
+
+        if (lowerCaseTest.Contains("darn") || lowerCaseTest.Contains("heck"))
+        {
+            output = true;
+        }
+
+        return output;
     }
 }
